73ef59ddb3b8c1b4bcf5a7b186218d237851bb8f
Dowon Lee <edowon0623@gmail.com>
2018-10-30 12:18:25 +0900

    Upload CMS files

--- myboard/cms/urls.py
+++ myboard/cms/urls.py
@@ -9,6 +9,14 @@ urlpatterns = [
     path('book/add/', views.book_edit, name='book_add'),
     path('book/modify/<int:book_id>/', views.book_edit, name='book_modify'),
     path('book/delete/<int:book_id>/', views.book_remove, name='book_remove'),
+<<<<<<< HEAD
+
+    path('impression/<int:book_id>/', views.ImpressionList.as_view(), name='impression_list'),
+    path('impression/add/<int:book_id>/', views.impression_edit, name='impression_add'),
+    path('impression/modify/<int:book_id>/<int:imporession_id>/', views.impression_edit, name='impression_modify'),
+    path('impression/delete/<int:book_id>/<int:imporession_id>/', views.impression_remove, name='impression_remove'),
+=======
+>>>>>>> 9e3938c344be5380c54385b01dfa4f6a0a867132
 ]
 
 
--- myboard/cms/views.py
+++ myboard/cms/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 
 from cms.models import Book, Impression
 from cms.forms import BookForm, ImpressionForm
+<<<<<<< HEAD
+from django.views.generic.list import ListView
+=======
+>>>>>>> 9e3938c344be5380c54385b01dfa4f6a0a867132
 
 # Create your views here.
 def book_list(request):
@@ -37,4 +41,29 @@ def book_edit(request, book_id=None):
         return render(request, 'cms/book_edit.html', dict(form=form, book_id=book_id))
 
 def book_remove(request, book_id):
+<<<<<<< HEAD
+    book = get_object_or_404(Book, pk=book_id)
+    book.delete()
+    return redirect('cms:book_list')
+
+class ImpressionList(ListView):
+    context_object_name = "impressions"
+    template_name = "cms/impresssion_list.html"
+    paginate_by = 2
+
+    def get(self, request, *args, **kwargs):
+        book = get_object_or_404(Book, pk=kwargs["book_id"])
+        impressions = book.impressions.all().order_by("id")
+        self.object_list = impressions
+        
+        context = self.get_context_data(object_list=self.object_list, book=book)
+        return self.render_to_response(context)
+
+def impression_edit(request, book_id, impress_id=None):    
+    return HttpResponse("감상평 수정/추가")
+
+def impression_remove(request, book_id, impress_id):    
+    return HttpResponse("감상평 삭제")    
+=======
     return HttpResponse('도서 삭제')
+>>>>>>> 9e3938c344be5380c54385b01dfa4f6a0a867132
